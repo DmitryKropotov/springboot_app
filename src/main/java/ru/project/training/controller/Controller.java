5b8e74e8a596010cfa0a0ab2a477074ab0110299
KropotovDA <kropotovd@gmail.com>
2021-09-15 09:03:18 +0300

    project attributes and log in file

--- src/main/java/ru/project/training/controller/Controller.java
+++ src/main/java/ru/project/training/controller/Controller.java
@@ -1,16 +1,20 @@
 package ru.project.training.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import ru.project.training.SpringBootTraining;
 @RestController
 @RequestMapping("/controller")
 public class Controller {
 
+        private final static Logger log = LoggerFactory.getLogger(SpringBootTraining.class);
+
         @GetMapping("")
         public int control() {
-                System.out.println("controller stb");
+                log.info("controller control");
                 return  0;
         }
 }
