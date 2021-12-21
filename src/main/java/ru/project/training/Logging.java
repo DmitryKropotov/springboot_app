7475c52fa848ef321a7fc3c58d004fc088a85f84
DKropotov <kropotovd@gmail.com>
2021-12-21 19:01:26 +0300

    more pointcuts

--- src/main/java/ru/project/training/Logging.java
+++ src/main/java/ru/project/training/Logging.java
@@ -14,12 +14,48 @@ public class Logging {
     private final static Logger log = LoggerFactory.getLogger(Logging.class);
 
     @Pointcut("execution(* ru.project.training.service.impl.StudentServiceImpl.findAll())")
-    public void pointcutTest() {
+    public void findAllPointcut() {
+    }
+
+    @Pointcut("execution(* ru.project.training.service.impl.StudentServiceImpl.*(..))")
+    public void studentSeviceImplPointcut() {
+    }
+
+    @Pointcut("within(ru.project.training..*)")
+    public void withinPointcut() {
+    }
+
+    @Pointcut("target(ru.project.training.service.impl.StudentServiceImpl)")
+    public void targetPointcut() {
+    }
+
+    @Pointcut("this(ru.project.training.service.StudentService)")
+    public void thisPointcut() {
     }
 
 
-    @Before("pointcutTest()")
-    public void logDatabaseInteraction(JoinPoint joinPoint) {
-        System.out.println("logDatabaseInteraction in Logging class");
+    @Before("findAllPointcut()")
+    public void adviceOne(JoinPoint joinPoint) {
+        System.out.println("findAllPointcut");
+    }
+
+    @Before("studentSeviceImplPointcut()")
+    public void adviceTwo(JoinPoint joinPoint) {
+        System.out.println("studentSeviceImplPointcut");
+    }
+
+    @Before("withinPointcut()")
+    public void adviceWithin() {
+        System.out.println("adviceWithin pointcut");
+    }
+
+    @Before("targetPointcut()")
+    public void adviceTarget() {
+        System.out.println("targetPointcut pointcut");
+    }
+
+    @Before("thisPointcut()")
+    public void adviceThis() {
+        System.out.println("thisPointcut pointcut");
     }
 }
